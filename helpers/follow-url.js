ffa24666e94065a3d5015f30ec695107423c4612
denoteit <ben@denote.it>
2019-11-13 10:25:26 +0000

    change - tidied followUrl fn.

--- helpers/follow-url.js
+++ helpers/follow-url.js
@@ -2,29 +2,17 @@ var http = require('http');
 var https = require('https'); 
 var url = require('url');
 
-async function followUrl (_url, _host = null, _trace = null) {
-
-  if (_trace === null) {
-    _trace = []
-  }
-
+async function followUrl (_url, _host = null, _trace = []) {
   return new Promise(async (resolve, reject) => {
     var httpProtocol = (_url.startsWith('https:')) ? https : http;
     httpProtocol.get(_url, async function (res) {
-  
       if (res.statusCode > 300 && res.statusCode < 400 && res.headers.location) {
-          let redirectUrl
           _trace.push({
             statusCode: res.statusCode,
             url: _url,
             redirectUrl: res.headers.location
           })
-          if (url.parse(res.headers.location).hostname) {
-            redirectUrl = res.headers.location
-          } else {
-            // @TODO Hostname not included; get host from requested URL (url.parse()) and prepend to location.
-          }
-          await followUrl(redirectUrl, null, _trace)
+          await followUrl(res.headers.location, null, _trace)
       } else {
         _trace.push({
           statusCode: res.statusCode,
